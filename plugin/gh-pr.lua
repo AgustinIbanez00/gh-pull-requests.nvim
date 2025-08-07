6606ad0d041ca191336549a0051fff47adea663f
Agustin Ibañez <48695094+AgustinIbanez00@users.noreply.github.com>
2025-08-07 12:34:44 -0300

    feat: add diff navigation and keymaps

--- plugin/gh-pr.lua
+++ plugin/gh-pr.lua
@@ -10,5 +10,19 @@ vim.api.nvim_create_user_command("GhPrList", function()
 end, {})
 
 vim.api.nvim_create_user_command("GhPrToggleReviewed", function()
-	require("gh-pr.pulls").toggle_reviewed()
+        require("gh-pr.pulls").toggle_reviewed()
 end, {})
+
+vim.api.nvim_create_user_command("GhPrNextChange", function()
+        require("gh-pr.pulls").next_change()
+end, {})
+
+vim.api.nvim_create_user_command("GhPrPrevChange", function()
+        require("gh-pr.pulls").prev_change()
+end, {})
+
+local map = vim.keymap.set
+map("n", "<leader>ghl", "<cmd>GhPrList<CR>", { desc = "List pull requests" })
+map("n", "<leader>ght", "<cmd>GhPrToggleReviewed<CR>", { desc = "Toggle reviewed" })
+map("n", "<leader>ghn", "<cmd>GhPrNextChange<CR>", { desc = "Next diff change" })
+map("n", "<leader>ghp", "<cmd>GhPrPrevChange<CR>", { desc = "Previous diff change" })
